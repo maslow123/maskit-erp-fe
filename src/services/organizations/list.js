2a1737287839265163bc0c87dcc0f07e758d4f1e
M Fadhly NR <fadhly@steradian.co.id>
2023-12-07 22:56:07 +0700

    Finish organizations CRUD

--- src/services/organizations/list.js
+++ src/services/organizations/list.js
@@ -2,64 +2,10 @@
 
 import { headers } from '../headers'
 
-const mock = [
-    {
-        id: 'string',
-        name: 'string',
-        business_field: 'string',
-        pic_name: 'string',
-        address: 'string',
-        email: 'string',
-        phone_number: 'string',
-        fax_number: 'string',
-        website: 'string',
-        bank_name: 'string',
-        bank_branch: 'string',
-        bank_acc_no: 'string',
-        created_at: '2023-11-26T04:39:42.093Z',
-        updated_at: '2023-11-26T04:39:42.093Z',
-        deleted_at: '2023-11-26T04:39:42.093Z',
-    },
-    {
-        id: 'string',
-        name: 'string',
-        business_field: 'string',
-        pic_name: 'string',
-        address: 'string',
-        email: 'string',
-        phone_number: 'string',
-        fax_number: 'string',
-        website: 'string',
-        bank_name: 'string',
-        bank_branch: 'string',
-        bank_acc_no: 'string',
-        created_at: '2023-11-26T04:39:42.093Z',
-        updated_at: '2023-11-26T04:39:42.093Z',
-        deleted_at: '2023-11-26T04:39:42.093Z',
-    },
-    {
-        id: 'string',
-        name: 'string',
-        business_field: 'string',
-        pic_name: 'string',
-        address: 'string',
-        email: 'string',
-        phone_number: 'string',
-        fax_number: 'string',
-        website: 'string',
-        bank_name: 'string',
-        bank_branch: 'string',
-        bank_acc_no: 'string',
-        created_at: '2023-11-26T04:39:42.093Z',
-        updated_at: '2023-11-26T04:39:42.093Z',
-        deleted_at: '2023-11-26T04:39:42.093Z',
-    },
-]
-
-const list = async (query) => {
+const list = async (page, limit, search) => {
     try {
         const data = await fetch(
-            `${process.env['NEXT_PUBLIC_API_SERVICE_URL']}/organizations`,
+            `${process.env['NEXT_PUBLIC_API_SERVICE_URL']}/organizations?page=${page}&offset=${limit}&name=${search}`,
             {
                 method: 'GET',
                 ...headers
